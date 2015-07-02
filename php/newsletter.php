da9c5b90dd906d758e23995f60eda09bd6a0ce44
James <james.barnes@moonpig.com>
2015-07-02 16:29:33 +0100

    Amendments to the newsletter php scripts.

--- php/newsletter.php
+++ php/newsletter.php
@@ -1,25 +1,20 @@
-<?php
-    $error_message = "";
- 
-    $email_to = "lindsey.gillespie@doggiesdaycareuk.com";
-    $email_subject = "Newsletter Subscription";
- 
+<?php 
     $first_name = $_POST['nlname'];
     $email_from = $_POST['nlemail'];
- 
+
+    $email_to = "lindsey.gillespie@doggiesdaycareuk.com";
+    $email_subject = "Newsletter Subscription";
     $email_message = "A newsletter subscription has been submitted.\n\n";
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
 
-function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
+    $headers = "From: $email_from.\n".
+    $headers .= "Reply-To: $email_from.";
+
+    @mail($email_to, $email_subject, $email_message, $headers);  
+
+    function clean_string($string) {
+          $bad = array("content-type","bcc:","to:","cc:","href");
+          return str_replace($bad,"",$string);
     }
- 
-$headers = 
-    'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
 ?>
