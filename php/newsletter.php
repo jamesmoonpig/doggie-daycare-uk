9202c78c205eb67c7c85da49c7d41f4021d5e834
James <james.barnes@moonpig.com>
2015-07-02 13:17:26 +0100

    New newsletter php. Amended action tag on form.

--- php/newsletter.php
+++ php/newsletter.php
@@ -1,24 +1,25 @@
 <?php
+    $error_message = "";
+ 
+    $email_to = "lindsey.gillespie@doggiesdaycareuk.com";
+    $email_subject = "Newsletter Subscription";
+ 
+    $first_name = $_POST['nlname'];
+    $email_from = $_POST['nlemail'];
+ 
+    $email_message = "A newsletter subscription has been submitted.\n\n";
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
 
-$name = $_POST['nlname'];
-$email_address = $_POST['nlemail'];
-	
-$to = 'lindsey.gillespie@doggiesdaycareuk.com'; 
-
-$email_subject = "New newsletter submission!";
-$email_body = "You have received a new newsletter submission from $name.\nContact email: $email_address";
-
-$headers = "From: newsletter@doggiesdaycareuk.com";
-$headers .= "Reply-To: $email_address";
-
-// Create the email and send the message
-$to = 'lindsey.gillespie@doggiesdaycareuk.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "New Newsletter Subscription:  $name";
-$email_body = "You have received a new newsletter subscription.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address";
-$headers = "From: newsletter@doggiesdaycareuk.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-
-return true;	
-
+function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+$headers = 
+    'From: '.$email_from."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
 ?>
