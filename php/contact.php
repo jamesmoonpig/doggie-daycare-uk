ed62a472a0d7666e2cd43588270e326ddeda7def
James Barnes <james.barnes@moonpig.com>
2015-09-07 22:16:50 +0100

    contact form fix - in progress

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,15 @@
 <?php
 
-$name = $_POST['iname'];
-$email_address = $_POST['iemail'];
-$phone = $_POST['iphone'];
-$message = $_POST['imessage'];
-	
-// Create the email and send the message
-$to = 'lindsey.gillespie@doggiesdaycareuk.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "New Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: contactform@doggiesdaycareuk.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;	
+$firstName = $_POST['firstName'];
+$Surname = $_POST['surname'];
+$message = $_POST['contactQuery'];
 
+$to      = 'info@doggiesdaycareuk.com';
+$subject = "New contact from $firstName $Surname";
+$body = "New message recieved: \n \n $message";
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $body, $headers);
 ?>
